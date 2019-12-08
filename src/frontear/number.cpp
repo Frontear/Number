21cb01b7c6765cb0e3b6a24628bf599d2b2d23d9
Ali Rizvi <frontear4594@gmail.com>
2019-12-07 22:38:28 -0500

    More definitions, add subtraction functionality
    I've written down what issues I am currently aware of, and will fix them once I implement all the functionality I want.

--- src/frontear/number.cpp
+++ src/frontear/number.cpp
@@ -1,10 +1,33 @@
 #include "number.h"
 #include <utility>
+#include <iostream>
 
+#define ZERO_PAD(str, len) if (str.length() < len) str = std::string(len - str.length(), '0') + str
+#define OPERATION(name) frontear::Number name(const std::string &x, const std::string &y, const long len)
+#define OPERATOR(sign, func) frontear::Number frontear::Number::operator sign(const frontear::Number &other) const {\
+    auto x = this->value, y = other.value;\
+    const auto len = std::max(x.length(), y.length());\
+    \
+    ZERO_PAD(x, len);\
+    ZERO_PAD(y, len);\
+    \
+    return func(x, y, len);\
+}
+
+/*
+ * Main issues: todo
+ *  - Does not check for string validity
+ *  - Performs dangerous std::move operation
+ */
 frontear::Number::Number(std::string number) : value(std::move(number)) {}
 
-frontear::Number add(const std::string &x, const std::string &y, const long len) {
-    std::string result(len + 1, '0'); // sum will never be greater than max length + 1
+/*
+ * Main issues: todo
+ *  - Cannot add negative numbers
+ *  - Cannot handle decimal values
+ */
+OPERATION(add) {
+    std::string result(len + 1, '0'); // result will never be greater than max length + 1
 
     auto res = result.rbegin();
     auto carry = 0u;
@@ -12,41 +35,61 @@ frontear::Number add(const std::string &x, const std::string &y, const long len)
         auto eval = (x[i] - '0') + (y[i] - '0') + carry;
 
         carry = 0u;
-        while (eval >= 10) {
+        if (eval >= 10) {
             eval -= 10;
-            ++carry;
+            carry = 1u;
         }
 
         *res = eval + '0';
     }
 
-    result = result.erase(0, std::min(result.find_first_not_of('0'), result.length() - 1)); // remove trailing 0s
-    return frontear::Number(result);
+    return frontear::Number(result.erase(0, std::min(result.find_first_not_of('0'), result.length() - 1))); // remove trailing 0s
 }
 
-frontear::Number frontear::Number::operator+(const frontear::Number &other) const {
-    auto x = this->value, y = other.value;
-    const auto len = std::max(x.length(), y.length());
+OPERATOR(+, add)
 
-#define ZERO_PAD(str, len) if (str.length() < len) str = std::string(len - str.length(), '0') + str
-    ZERO_PAD(x, len);
-    ZERO_PAD(y, len);
-#undef ZERO_PAD
+/*
+ * Main issues: todo
+ *  - Cannot subtract negative values
+ *  - Cannot handle decimal values
+ *  - Cannot handle y being greater than x (only has max - min subtraction)
+ *  - Narrow conversions between unsigned and signed ints
+ */
+OPERATION(sub) {
+    std::string result(len, '0'); // result will never be greater than the max length
 
-    return add(x, y, len);
+    auto res = result.rbegin();
+    auto borrow = 0u;
+    for (auto i = len - 1; i >= 0; --i, ++res) {
+        int first = x[i] - '0', second = y[i] - '0';
+
+        if (borrow > 0) {
+            first -= borrow;
+            borrow = 0u;
+        }
+        if (second > first) {
+            first += 10 * (borrow = 1);
+        }
+
+        *res = (first - second) + '0';
+    }
+
+    return frontear::Number(result.erase(0, std::min(result.find_first_not_of('0'), result.length() - 1))); // remove trailing 0s
 }
 
-frontear::Number frontear::Number::operator-(const frontear::Number &other) const {
-    return Number("0");
-}
+OPERATOR(-, sub)
 
 frontear::Number frontear::Number::operator*(const frontear::Number &other) const {
-    return Number("0");
+    return frontear::Number("0");
 }
 
 frontear::Number frontear::Number::operator/(const frontear::Number &other) const {
-    return Number("0");
+    return frontear::Number("0");
 }
+
+#undef OPERATOR
+#undef OPERATION
+#undef ZERO_PAD
 
 std::ostream &frontear::operator<<(std::ostream &os, const frontear::Number &number) {
     os << number.value;
--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,8 @@ int main() {
 
     cout << x << " + " << y << " = " << x + y << endl;
     cout << x << " - " << y << " = " << x - y << endl;
-    cout << x << " * " << y << " = " << x * y << endl;
-    cout << x << " / " << y << " = " << x / y << endl;
+    //cout << x << " * " << y << " = " << x * y << endl;
+    //cout << x << " / " << y << " = " << x / y << endl;
 
     cout << endl;
 
